dcd3683d28886179d46e03dc4f0ccc864f56d5af
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2025-01-08 23:37:24 +0700

    feat: calendar page

---
+++ lib/screens/task_detail.dart
@@ -0,0 +1,220 @@
+// ignore_for_file: unused_element, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:task_manager/widgets/calendar_dates.dart';
+import 'package:task_manager/widgets/task_container.dart';
+
+class TaskDetailScreen extends StatefulWidget {
+  const TaskDetailScreen({super.key});
+
+  @override
+  State<TaskDetailScreen> createState() => _TaskDetailScreenState();
+}
+
+class _TaskDetailScreenState extends State<TaskDetailScreen> {
+  final List<String> days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  final List<int> dates = [20, 21, 22, 23, 24, 25, 26];
+  final List<int> time = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
+  static CircleAvatar calendarIcon() {
+    return CircleAvatar(
+      radius: 25.0,
+      backgroundColor: Colors.white,
+      child: Icon(
+        Icons.calendar_today,
+        size: 20.0,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _dashedText() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15),
+      child: Text(
+        '------------------------------------------',
+        maxLines: 1,
+        style:
+            TextStyle(fontSize: 20.0, color: Colors.black12, letterSpacing: 5),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.yellow[50],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(
+            20,
+            20,
+            20,
+            0,
+          ),
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Today',
+                      style: TextStyle(
+                          fontSize: 30.0, fontWeight: FontWeight.w700),
+                    ),
+                    Container(
+                      height: 40.0,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => CreateNewTaskPage(),
+                          //   ),
+                          // );
+                        },
+                        child: Center(
+                          child: Text(
+                            'Add task',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Productive Day, Sourav',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'April, 2020',
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                height: 58.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: days.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CalendarDates(
+                      day: days[index],
+                      date: dates[index].toString(),
+                      dayColor: index == 0 ? Colors.red : Colors.black54,
+                      dateColor: index == 0 ? Colors.red : Colors.blue,
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: ListView.builder(
+                            itemCount: time.length,
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemBuilder: (BuildContext context, int index) =>
+                                Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 15.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  '${time[index]} ${time[index] > 8 ? 'PM' : 'AM'}',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: ListView(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            children: <Widget>[
+                              _dashedText(),
+                              TaskContainer(
+                                title: 'Project Research',
+                                subtitle:
+                                    'Discuss with the colleagues about the future plan',
+                                boxColor: Colors.yellow,
+                              ),
+                              _dashedText(),
+                              TaskContainer(
+                                title: 'Work on Medical App',
+                                subtitle: 'Add medicine tab',
+                                boxColor: Colors.purple,
+                              ),
+                              TaskContainer(
+                                title: 'Call',
+                                subtitle: 'Call to david',
+                                boxColor: Colors.pink,
+                              ),
+                              TaskContainer(
+                                title: 'Design Meeting',
+                                subtitle:
+                                    'Discuss with designers for new task for the medical app',
+                                boxColor: Colors.green,
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
